ef6091265677ad91e0eb1c08bb55d9abcdad8154
JCW-WANG <41102005e@gapps.ntnu.edu.tw>
2025-07-01 11:14:24 +0800

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,5 +1,5 @@
 const canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
+document.body.insertBefore(canvas, document.body.firstChild);
 canvas.style.position = 'fixed';
 canvas.style.top = '0';
 canvas.style.left = '0';
@@ -24,26 +24,36 @@ class Particle {
     this.x = Math.random()*width;
     this.y = Math.random()*height;
     this.radius = Math.random()*1.5 + 0.5;
-    this.vx = (Math.random()-0.5)*0.15;
-    this.vy = (Math.random()-0.5)*0.15;
+    this.vx = (Math.random()-0.5)*0.2;
+    this.vy = (Math.random()-0.5)*0.2;
   }
   update() {
     this.x += this.vx;
     this.y += this.vy;
-    if(this.x<0||this.x>width) this.vx*=-1;
-    if(this.y<0||this.y>height) this.vy*=-1;
+    if(this.x < 0 || this.x > width) this.vx = -this.vx;
+    if(this.y < 0 || this.y > height) this.vy = -this.vy;
   }
-  draw() {
+  draw(ctx) {
     ctx.beginPath();
-    ctx.fillStyle = 'rgba(150,150,150,0.1)';
-    ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+    ctx.fillStyle = 'rgba(100,100,100,0.15)';
+    ctx.shadowColor = 'rgba(255,255,255,0.3)';
+    ctx.shadowBlur = 2;
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
     ctx.fill();
   }
 }
-for(let i=0;i<100;i++) particles.push(new Particle());
+
+for(let i=0; i<100; i++) {
+  particles.push(new Particle());
+}
+
 function animate() {
   ctx.clearRect(0,0,width,height);
-  particles.forEach(p=>{p.update();p.draw();});
+  particles.forEach(p => {
+    p.update();
+    p.draw(ctx);
+  });
   requestAnimationFrame(animate);
 }
+
 animate();
