f0ae926fcb11e56b69e7f2273c16d6954ae7c062
JCW-WANG <41102005e@gapps.ntnu.edu.tw>
2025-07-01 14:53:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const thumbnails = [
-  "https://picsum.photos/id/1011/600/400",
-  "https://picsum.photos/id/1012/600/400",
-  "https://picsum.photos/id/1013/600/400",
-  "https://picsum.photos/id/1015/600/400"
+  "https://picsum.photos/id/1011/300/200",
+  "https://picsum.photos/id/1012/300/200",
+  "https://picsum.photos/id/1013/300/200",
+  "https://picsum.photos/id/1015/300/200"
 ];
+
 const buttonsContainer = document.getElementById("buttons");
 
 function createButtons() {
@@ -11,8 +12,10 @@ function createButtons() {
   thumbnails.forEach((thumb, index) => {
     const btn = document.createElement("button");
     btn.innerHTML = `
-      <img src="${thumb}" alt="作品 ${index+1}">
-      <div class="label">作品 ${index+1}</div>
+      <img src="${thumb}" alt="作品 ${index + 1}" />
+      <div class="label" style="background:transparent; color:#222; text-shadow:none;">
+        作品 ${index + 1}
+      </div>
     `;
     btn.addEventListener("click", () => {
       window.location.href = `video.html?index=${index}`;
@@ -20,4 +23,5 @@ function createButtons() {
     buttonsContainer.appendChild(btn);
   });
 }
+
 createButtons();
